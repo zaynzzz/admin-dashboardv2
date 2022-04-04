a3be1fec66f19d3dbc5260e3eb32f9c8a9390a3d
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-04 08:29:07 +0700

    Initial commit

---
+++ application/controllers/Roles.php
@@ -0,0 +1,265 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+require APPPATH . '/libraries/BaseController.php';
+
+/**
+ * Class : Roles (RolesController)
+ * Roles Class to control role related operations.
+ * @author : Kishor Mali
+ * @version : 1.1
+ * @since : 22 Jan 2021
+ */
+class Roles extends BaseController
+{
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('role_model', 'rm');
+        $this->isLoggedIn();   
+    }
+
+    /**
+     * This is default routing method
+     * It routes to default listing page
+     */
+    public function index()
+    {
+        redirect('roleListing/');
+    }
+    
+    /**
+     * This function is used to load the role list
+     */
+    function roleListing()
+    {
+        if(!$this->isAdmin())
+        {
+            $this->loadThis();
+        }
+        else
+        {        
+            $searchText = $this->security->xss_clean($this->input->post('searchText'));
+            $data['searchText'] = $searchText;
+            
+            $this->load->library('pagination');
+            
+            $count = $this->rm->roleListingCount($searchText);
+
+			$returns = $this->paginationCompress ( "roleListing/", $count, 10 );
+            
+            $data['roleRecords'] = $this->rm->roleListing($searchText, $returns["page"], $returns["segment"]);
+            
+            $this->global['pageTitle'] = 'CodeInsect : Roles Listing';
+            
+            $this->loadViews("roles/list", $this->global, $data, NULL);
+        }
+    }
+
+    /**
+     * This function is used to load the add new form
+     */
+    function add()
+    {
+        if(!$this->isAdmin())
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->global['pageTitle'] = 'CodeInsect : Add New Role';
+
+            $this->loadViews("roles/add", $this->global, NULL, NULL);
+        }
+    }
+
+    /**
+     * This function is used to check whether email already exist or not
+     */
+    function checkRoleExists()
+    {
+        $userId = $this->input->post("userId");
+        $email = $this->input->post("email");
+
+        if(empty($userId)){
+            $result = $this->user_model->checkEmailExists($email);
+        } else {
+            $result = $this->user_model->checkEmailExists($email, $userId);
+        }
+
+        if(empty($result)){ echo("true"); }
+        else { echo("false"); }
+    }
+    
+    /**
+     * This function is used to add new user to the system
+     */
+    function addNewRole()
+    {
+        if(!$this->isAdmin())
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->load->library('form_validation');
+            
+            $this->form_validation->set_rules('role','Role Text','trim|required|max_length[50]');
+            $this->form_validation->set_rules('status','Status','trim|required|numeric');
+            
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->add();
+            }
+            else
+            {
+                $roleText = $this->security->xss_clean($this->input->post('role'));
+                $status = $this->security->xss_clean($this->input->post('status'));
+                
+                $roleInfo = array('role'=>$roleText, 'status'=>$status, 'createdBy'=>$this->vendorId, 'createdDtm'=>date('Y-m-d H:i:s'));
+                
+                $result = $this->rm->addNewRole($roleInfo);
+                
+                if($result > 0)
+                {
+                    $this->addRoleMatrix($result);
+                    $this->session->set_flashdata('success', 'New Role created successfully');
+                }
+                else
+                {
+                    $this->session->set_flashdata('error', 'Role creation failed');
+                }
+                
+                redirect('roles/roleListing');
+            }
+        }
+    }
+
+    
+    /**
+     * This function is used load user edit information
+     * @param number $roleId : Optional : This is user id
+     */
+    function edit($roleId = NULL)
+    {
+        if(!$this->isAdmin())
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            if($roleId == null)
+            {
+                redirect('roles/roleListing');
+            }
+            
+            $data['roleInfo'] = $this->rm->getRoleInfo($roleId);
+            $roleAccessMatrix = $this->rm->getRoleAccessMatrix($roleId);
+            $data['roleAccessMatrix'] = json_decode($roleAccessMatrix->access);
+            $data['moduleList'] = $this->config->item('moduleList');
+
+            // pre($data); die;
+            
+            $this->global['pageTitle'] = 'CodeInsect : Edit Role';
+            
+            $this->loadViews("roles/edit", $this->global, $data, NULL);
+        }
+    }
+    
+    
+    /**
+     * This function is used to edit the user information
+     */
+    function editRole()
+    {
+        if(!$this->isAdmin())
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->load->library('form_validation');
+            
+            $roleId = $this->input->post('roleId');
+            
+            $this->form_validation->set_rules('role','Role Text','trim|required|max_length[50]');
+            $this->form_validation->set_rules('status','Status','trim|required|numeric');
+            
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->edit($roleId);
+            }
+            else
+            {
+                $roleText = $this->security->xss_clean($this->input->post('role'));
+                $status = $this->security->xss_clean($this->input->post('status'));
+                
+                $roleInfo = array('role'=>$roleText, 'status'=>$status, 'updatedBy'=>$this->vendorId, 'updatedDtm'=>date('Y-m-d H:i:s'));
+                
+                $result = $this->rm->editRole($roleInfo, $roleId);
+                
+                if($result == true)
+                {
+                    $this->session->set_flashdata('success', 'Role updated successfully');
+                }
+                else
+                {
+                    $this->session->set_flashdata('error', 'Role updation failed');
+                }
+                
+                redirect('roles/roleListing');
+            }
+        }
+    }
+
+    private function addRoleMatrix($roleId)
+    {
+        $this->load->config('modules');
+
+        $modules = $this->config->item('moduleList');
+
+        $accessMatrix = array('roleId'=>$roleId, 'access'=>json_encode($modules), 'createdBy'=> $this->vendorId, 'createdDtm'=>date('Y-m-d H:i:s'));
+
+        $this->rm->insertAccessMatrix($accessMatrix);
+    }
+
+    public function storeAccessMatrix()
+    {
+        $roleId = $this->input->post('roleIdForMatrix');
+        $postParams = $this->input->post('access');
+
+        $this->load->config('modules');
+
+        $modules = $this->config->item('moduleList');
+        $modules2 = [];
+
+        foreach($modules as $module) {
+            $singleModule = ['module'=>$module['module']];
+            foreach($module as $keyMod=>$valMod) {
+                if(isset($postParams[$module['module']][$keyMod])) {
+                    $singleModule[$keyMod] = $postParams[$module['module']][$keyMod] == 'on' ? 1 : $postParams[$module['module']][$keyMod];
+                } else {
+                    $singleModule[$keyMod] = 0;
+                }
+            }
+            $modules2[] = $singleModule;
+        }
+
+        $accessMatrix = ['access'=>json_encode($modules2), 'updatedBy'=>$this->vendorId, 'updatedDtm'=>date('Y-m-d H:i:s')];
+
+        $updated = $this->rm->updateAccessMatrix($roleId, $accessMatrix);
+
+        if($updated){
+            $this->session->set_flashdata('success', 'Access matrix updated successfully');
+        } else {
+            $this->session->set_flashdata('error', 'Access matrix updation failed');
+        }
+
+        redirect('roles/edit/'.$roleId);
+    }
+}
+
+
+?>
---
+++ application/models/Role_model.php
@@ -0,0 +1,228 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Class : Role_model (Role Model)
+ * User model class to get to handle role related data 
+ * @author : Kishor Mali
+ * @version : 1.2
+ * @since : 22 Jan 2021
+ */
+class Role_model extends CI_Model
+{
+    /**
+     * This function is used to get the role listing count
+     * @param string $searchText : This is optional search text
+     * @return number $count : This is row count
+     */
+    function roleListingCount($searchText = '')
+    {
+        $this->db->select('BaseTbl.roleId, BaseTbl.role, BaseTbl.status, BaseTbl.createdDtm');
+        $this->db->from('tbl_roles as BaseTbl');
+        if(!empty($searchText)) {
+            $likeCriteria = "(BaseTbl.role  LIKE '%".$searchText."%')";
+            $this->db->where($likeCriteria);
+        }
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $query = $this->db->get();
+        
+        return $query->num_rows();
+    }
+    
+    /**
+     * This function is used to get the role listing count
+     * @param string $searchText : This is optional search text
+     * @param number $page : This is pagination offset
+     * @param number $segment : This is pagination limit
+     * @return array $result : This is result
+     */
+    function roleListing($searchText = '', $page, $segment)
+    {
+        $this->db->select('BaseTbl.roleId, BaseTbl.role, BaseTbl.status, BaseTbl.createdDtm');
+        $this->db->from('tbl_roles as BaseTbl');
+        if(!empty($searchText)) {
+            $likeCriteria = "(BaseTbl.role  LIKE '%".$searchText."%')";
+            $this->db->where($likeCriteria);
+        }
+        $this->db->where('BaseTbl.isDeleted', 0);
+        $this->db->order_by('BaseTbl.roleId', 'DESC');
+        $this->db->limit($page, $segment);
+        $query = $this->db->get();
+        
+        $result = $query->result();        
+        return $result;
+    }
+    
+    /**
+     * This function is used to get the user roles information
+     * @return array $result : This is result of the query
+     */
+    function getUserRoles()
+    {
+        $this->db->select('roleId, role');
+        $this->db->from('tbl_roles');
+        $this->db->where('roleId !=', 1);
+        $query = $this->db->get();
+        
+        return $query->result();
+    }
+
+    /**
+     * This function is used to check whether email id is already exist or not
+     * @param {string} $email : This is email id
+     * @param {number} $userId : This is user id
+     * @return {mixed} $result : This is searched result
+     */
+    function checkEmailExists($email, $userId = 0)
+    {
+        $this->db->select("email");
+        $this->db->from("tbl_users");
+        $this->db->where("email", $email);   
+        $this->db->where("isDeleted", 0);
+        if($userId != 0){
+            $this->db->where("userId !=", $userId);
+        }
+        $query = $this->db->get();
+
+        return $query->result();
+    }
+    
+    
+    /**
+     * This function is used to add new role to system
+     * @return number $insert_id : This is last inserted id
+     */
+    function addNewRole($roleInfo)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_roles', $roleInfo);
+        
+        $insert_id = $this->db->insert_id();
+        
+        $this->db->trans_complete();
+        
+        return $insert_id;
+    }
+    
+    /**
+     * This function used to get role information by id
+     * @param number $roleId : This is role id
+     * @return array $result : This is role information
+     */
+    function getRoleInfo($roleId)
+    {
+        $this->db->select('roleId, role, status');
+        $this->db->from('tbl_roles');
+        $this->db->where('roleId', $roleId);
+        $this->db->where('isDeleted', 0);
+        $query = $this->db->get();
+        
+        return $query->row();
+    }
+    
+    
+    /**
+     * This function is used to update the role information
+     * @param array $roleInfo : This is role updated information
+     * @param number $roleId : This is role id
+     */
+    function editRole($roleInfo, $roleId)
+    {
+        $this->db->where('roleId', $roleId);
+        $this->db->update('tbl_roles', $roleInfo);
+        
+        return TRUE;
+    }
+    
+    
+    
+    /**
+     * This function is used to delete the user information
+     * @param number $userId : This is user id
+     * @return boolean $result : TRUE / FALSE
+     */
+    function deleteUser($userId, $userInfo)
+    {
+        $this->db->where('userId', $userId);
+        $this->db->update('tbl_users', $userInfo);
+        
+        return $this->db->affected_rows();
+    }
+
+
+    /**
+     * This function used to get access matrix of a role by roleId
+     * @param number $roleId : This is roleId of user
+     */
+    function getRoleAccessMatrix($roleId)
+    {
+        $this->db->select('roleId, access');
+        $this->db->from('tbl_access_matrix');
+        $this->db->where('roleId', $roleId);
+        $query = $this->db->get();
+        
+        $result = $query->row();
+        return $result;
+    }
+
+
+    function insertAccessMatrix($accessMatrix)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_access_matrix', $accessMatrix);
+        $this->db->trans_complete();
+    }
+
+    function getFromAccessMatrix2()
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_access_matrix');
+        $this->db->where('roleId', 1);
+        $query = $this->db->get();
+        
+        $result = $query->row();
+        return $result;
+    }
+
+    function generateMatrix()
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_roles');
+        $query = $this->db->get();
+        
+        $roles = $query->result();
+
+        if(empty($result))
+        {
+            foreach($roles as $role)
+            {
+                $this->db->select('*');
+                $this->db->from('tbl_access_matrix');
+                $this->db->where('roleId', $role->roleId);
+                $query2 = $this->db->get();
+
+                $accessMatrices = $query2->result();
+
+                if(empty($accessMatrices))
+                {
+                    $modules = array(        
+                        array('module'=>'Task', 'total_access'=>0, 'list'=>0,  'create_records'=>0,  'edit_records'=>0,  'delete_records'=>0),
+                        array('module'=>'Bookings', 'total_access'=>0, 'list'=>0,  'create_records'=>0,  'edit_records'=>0,  'delete_records'=>0)
+                    );
+                    $accessMatrix = array('roleId'=> $role->roleId, 'access'=>json_encode($modules), 'createdBy'=> 1, 'createdDtm'=>date('Y-m-d H:i:s'));
+
+                    $this->insertAccessMatrix($accessMatrix);
+                }
+            }
+        }
+    }
+
+    function updateAccessMatrix($roleId, $accessMatrix)
+    {
+        $this->db->where('roleId', $roleId);
+        $this->db->update('tbl_access_matrix', $accessMatrix);
+
+        return $this->db->affected_rows();
+    }
+}
+
+  
